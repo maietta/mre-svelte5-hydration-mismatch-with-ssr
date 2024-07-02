dd9ca01b5f943754812957c7bd0e202f5ed9bde1
Nick Maietta <nick@maietta.org>
2024-07-02 16:50:52 -0700

    An attempt was made.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<a href="/">Main Page</a>
+
+<a href="/about">About</a>
+<a href="/contact">Contact</a>
+
+{@render children()}
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,5 @@
+<p>This is a contact page</p>
+
+<label for="email">Email
+    <input id="email" name="email" />
+</label>
